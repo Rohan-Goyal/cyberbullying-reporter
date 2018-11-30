dab3b513998dc71d3f7e70ef7a0a038be7a4bc0b
Rohan-Goyal <45487680+Rohan-Goyal@users.noreply.github.com>
2018-11-30 17:15:51 +0400

    Add files via upload
    Initial Commit: The raw files and the packed CRX

---
+++ background.js
@@ -0,0 +1,16 @@
+﻿
+  
+  chrome.runtime.onInstalled.addListener(function() {
+    console.log('I have awoken.'); 
+    });
+
+chrome.browserAction.onClicked.addListener(function() {
+	// find the current tab, and execute the inject.js file
+	chrome.tabs.query({active: true, lastFocusedWindow: true
+        }, function(tabs) 
+        {
+ 
+            var tab = tabs[0];
+	chrome.tabs.executeScript(null,{file:'inject.js'})
+		});
+});
---
+++ lp.js
@@ -0,0 +1,9 @@
+
+function pLPCommand(c, a1, a2, a3, a4) {
+parent.window["pLPCommand1"] && parent.window["pLPCommand1"](c, a1, a2, a3, a4);
+}
+function pRTLPCB(pN, data) {
+parent.window["pRTLPCB1"] && parent.window["pRTLPCB1"](pN, data);
+}
+         pLPCommand('start','143448','4SMwL2okkL');
+pRTLPCB(0,[{"t":"c","d":{"t":"h","d":{"ts":1542858353725,"v":"5","h":"s-usc1c-nss-216.firebaseio.com","s":"iTZBhanKJT96ydvhe0ErAS8ynP3SocPB"}}}]);
